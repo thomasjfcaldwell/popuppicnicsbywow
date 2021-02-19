6989f11dd6a3d023c881779933c7d7dbeaa39445
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2021-02-19 13:28:44 -0800

    update to slide images

--- src/Components/SliderData.js
+++ src/Components/SliderData.js
@@ -1,14 +1,14 @@
 export const SliderData = [
 	{
 		id: 1,
-		image: 'https://i.ibb.co/BnnZwtP/dark-Picnic.jpg',
+		image: 'https://i.ibb.co/5nSMRx2/new-Slide-Image1.jpg',
 	},
 	{
 		id: 2,
-		image: 'https://i.ibb.co/wdyN5nC/beachimage.jpg',
+		image: 'https://i.ibb.co/99snh7R/New-Slide-Image-2.jpg',
 	},
 	{
 		id: 3,
-		image: 'https://i.ibb.co/4tH4yN7/Table-Image.jpg',
+		image: 'https://i.ibb.co/Lk0TW9P/New-Slide-Image-3.jpg',
 	},
 ];
