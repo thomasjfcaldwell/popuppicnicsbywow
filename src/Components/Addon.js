52d7a8684f56d95223c81cfc7192ad4299d0f4ec
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2021-03-15 16:01:20 -0700

    addon edits

--- src/Components/Addon.js
+++ src/Components/Addon.js
@@ -51,24 +51,24 @@ class Addon extends Component {
 							textAlign: 'center',
 						}}>
 						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<Card.Img className='addonimg' src={blanket} />
+							<Image className='addonimg' src={blanket} />
 							<p className='serviceHeader'>Cozy Blanket Basket</p>
 							<h5 className='pricing'> $25</h5>
 						</Col>
 						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<Card.Img className='addonimg' src={Beverages} />
+							<Image className='addonimg' src={Beverages} />
 							<p className='serviceHeader'>Beverage Package for 2</p>
 							<h5 className='pricing'>$30</h5>
 						</Col>
 						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<Card.Img className='addonimg' src={Beverages1} />
+							<Image className='addonimg' src={Beverages1} />
 							<p className='serviceHeader'>
 								Additional Beverages for One Guest
 							</p>
 							<h5 className='pricing'>$5</h5>
 						</Col>
 						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<Card.Img className='addonimg' src={Teepee} />
+							<Image className='addonimg' src={Teepee} />
 							<p className='serviceHeader'>Teepee Set-Up</p>
 							<h5 className='pricing'>$40</h5>
 						</Col>
@@ -83,22 +83,22 @@ class Addon extends Component {
 							textAlign: 'center',
 						}}>
 						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<Card.Img className='addonimg' src={Umbrella} />
+							<Image className='addonimg' src={Umbrella} />
 							<p className='serviceHeader'>Posh Umbrella with Tassels</p>
 							<h5 className='pricing'>$45</h5>
 						</Col>
 						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<Card.Img className='addonimg' src={Dessert} />
+							<Image className='addonimg' src={Dessert} />
 							<p className='serviceHeader'>Dessert Platter </p>
 							<h5 className='pricing'>$40</h5>
 						</Col>
 						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<Card.Img className='addonimg' src={cheeseBox} />
+							<Image className='addonimg' src={cheeseBox} />
 							<p className='serviceHeader'>Cheese Charcuterie Box</p>
 							<h5 className='pricing'>$50</h5>
 						</Col>
 						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<Card.Img className='addonimg' src={Multi} />
+							<Image className='addonimg' src={Multi} />
 							<p className='serviceHeader'>Vegan/Vegetarian Charcuterie Box</p>
 							<h5 className='pricing'>$45</h5>
 						</Col>
@@ -113,22 +113,22 @@ class Addon extends Component {
 							textAlign: 'center',
 						}}>
 						<Col xs={6} sm={6} md={4} lg={3} className='addonCol'>
-							<Card.Img className='addonimg' src={MeatCheese} />
+							<Image className='addonimg' src={MeatCheese} />
 							<p className='serviceHeader'>Meat & Cheese Charcutier Box</p>
 							<h5 className='pricing'>$55</h5>
 						</Col>
 						<Col xs={6} sm={6} md={4} lg={3} className='addonCol'>
-							<Card.Img className='addonimg' src={Music} />
+							<Image className='addonimg' src={Music} />
 							<p className='serviceHeader'>Live Music Entertainment</p>
 							<h5 className='pricing'>$300+</h5>
 						</Col>
 						<Col xs={6} sm={6} md={4} lg={3} className='addonCol'>
-							<Card.Img className='addonimg' src={extraHour} />
+							<Image className='addonimg' src={extraHour} />
 							<p className='serviceHeader'>Additional Hour for Picnic</p>
 							<h5 className='pricing'>$75</h5>
 						</Col>
 						<Col xs={6} sm={6} md={4} lg={3} className='addonCol'>
-							<Card.Img className='addonimg' src={speaker} />
+							<Image className='addonimg' src={speaker} />
 							<p className='serviceHeader'>BlueTooth Speaker</p>
 							<h5 className='pricing'>$10</h5>
 						</Col>
@@ -175,7 +175,8 @@ class Addon extends Component {
 								fontFamily: 'Montserrat',
 								fontWeight: '500',
 								color: '#727272',
-								width: '75%',
+								width: '100%',
+								marginTop: '20px',
 							}}>
 							20% Service Fee,Sales tax & Applicable Delivery Fee will be added
 							to Pricing
