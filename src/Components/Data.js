76e8df9e0476ddefaf0614383dfca95e144b18f3
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2021-06-11 10:39:35 -0700

    updates on images

---
+++ src/Components/Data.js
@@ -0,0 +1,126 @@
+export const addOnData = [
+	{
+		id: '1',
+		title: 'COZY BLANKET BASKET',
+		image: 'https://i.ibb.co/ZmRy52s/blankets-copy.jpg',
+		description: '3 cozy blankets for the warm but fresh Californian evenings',
+		price: 25,
+	},
+	{
+		id: '2',
+		title: 'BEVERAGE PACKAGE FOR 2',
+		image: 'https://i.ibb.co/31d4Ltq/beveragepackage3-31-2021.jpg',
+		alt: 'beveragepackagefor2',
+		description: '4 beverages for 2 people to share',
+		price: 30,
+	},
+	{
+		id: '3',
+		title: 'ADDITIONAL BEVERAGES FOR ONE GUEST',
+		image: 'https://i.ibb.co/vZDHCWH/beveragepackage1.jpg',
+		description: 'Extra 2 beverages for an additional guest',
+		price: 5,
+	},
+	{
+		id: '4',
+		title: 'TEEPEE SET-UP',
+		image: 'https://i.ibb.co/J7300Vb/Tee-Pee-for-site.jpg',
+		description: 'A decorated Teepee which includes pillows',
+		price: 40,
+	},
+	{
+		id: '5',
+		title: 'POSH UMBRELLA WITH TASSELS',
+		image: 'https://i.ibb.co/dcZ8x57/Umbrella-For-Web.jpg',
+		description:
+			'Keep the hot Californian sun off you and guests with an Umbrella',
+		price: 45,
+	},
+	{
+		id: '6',
+		title: 'DESSERT PLATTER',
+		image: 'https://i.ibb.co/FD26JGc/dessert-Box.jpg',
+		description: 'Share a dessert platter with your guests',
+		price: 40,
+	},
+	{
+		id: '7',
+		title: 'CHEESE CHARCUTERIE BOX',
+		image: 'https://i.ibb.co/NyhqmHh/cheese-Box.jpg',
+		description:
+			'Cheese charcuterie box with a mixture of different cheeses and crackers',
+		price: 50,
+	},
+	{
+		id: '8',
+		title: 'VEGAN/VEGETARIAN CHARCUTERIE BOX',
+		image: 'https://i.ibb.co/CKvqyLc/veggie-Box.jpg',
+		description:
+			'Vegan charcuterie box with a mixture of different meats,cheeses, crackers and fruits',
+		price: 55,
+	},
+	{
+		id: '9',
+		title: 'MEAT & CHEESE CHARCUTERIE BOX',
+		image: 'https://i.ibb.co/MSC4JT0/meat-Chesse-Box.jpg',
+		description:
+			'Meat and Cheese charcuterie box with a mixture of different veggies, crackers and fruits',
+		price: 45,
+	},
+	{
+		id: '10',
+		title: 'LIVE MUSIC ENTERTAINMENT',
+		image: 'https://i.ibb.co/8xwNgxb/livemusic.jpg',
+		description:
+			'We can help bring a musical vibe to your picnic with live music! Prices start at $300 for a live performance at your picnic',
+		price: 300,
+	},
+	{
+		id: '11',
+		title: 'ADDITIONAL HOUR FOR PICNIC',
+		image: 'https://i.ibb.co/TK3dq8q/extrahour.jpg',
+		description:
+			'The duration of our picnics are 2 hours and 1.5 for the Basic Beauty package. Not long enough for you add an extra or 2 at $75 per hour',
+		price: 75,
+	},
+	{
+		id: '12',
+		title: 'BLUETOOTH SPEAKER',
+		image: 'https://i.ibb.co/B2DRJfn/Blue-Tooth-for-site.jpg',
+		description:
+			'Have a special soundtrack or music you would like to listen to during your picnic. Add a bluetooth speaker to make that happen',
+		price: 10,
+	},
+	{
+		id: '13',
+		title: 'ONE BALLOON GARLAND FOR TEEPEE SET-UP',
+		image: 'https://i.ibb.co/SKQyDQ8/ballon-Garland-4-9-2021.jpg',
+		description:
+			'Decorate a TeePee with a balloon garland.(teepee not included)',
+		price: 225,
+	},
+	{
+		id: '14',
+		title: 'SMORES BOXES (Serves 4 guests)',
+		image: 'https://i.ibb.co/QcgDc4n/smores.jpg',
+		description:
+			'Smores box thats serves 4 guests - Includes everything needed to enjoy smores on your picnic',
+		price: 50,
+	},
+	{
+		id: '15',
+		title: 'SMORES BOXES (Serves 12 guests)',
+		image: 'https://i.ibb.co/QcgDc4n/smores.jpg',
+		description:
+			'Smores box thats serves 4 guests - Includes everything needed to enjoy smores on your picnic',
+		price: 150,
+	},
+	{
+		id: '16',
+		title: 'HEART SHAPED BOX',
+		image: 'https://i.ibb.co/zbtzM8G/VDay-Dessert-Box2.jpg',
+		description:
+			'Sharing a picnic with someone special? Surprise them with this Heart Shaped Box filled with sweet treats ',
+		price: 45,
+	},
+];
--- src/Components/Pages/PicnicPage/Addon.js
+++ src/Components/Pages/PicnicPage/Addon.js
@@ -1,229 +1,67 @@
-import React, { Component } from 'react';
-import { Row, Col,  Container, Image } from 'react-bootstrap';
+import React from 'react';
+import { addOnData } from '../../Data';
+import { Row, Col, Container, Image, CardDeck, Card } from 'react-bootstrap';
 import '../../../styles/addon.css';
-import blanket from '../../../Images/Packages/blankets.jpg';
-import Beverages from '../../../Images/Packages/beveragepackage3.31.2021.jpg';
-import Teepee from '../../../Images/Packages/TeePee for site.jpg';
-import Umbrella from '../../../Images/Packages/UmbrellaForWeb.jpg';
-import Dessert from '../../../Images/Packages/dessertBox.jpg';
-import Multi from '../../../Images/Packages/veggieBox.jpg';
-import cheeseBox from '../../../Images/Packages/cheeseBox.jpg';
-import MeatCheese from '../../../Images/Packages/meatChesseBox.jpg';
-import Music from '../../../Images/Packages/livemusic.jpg';
-import speaker from '../../../Images/Packages/BlueTooth for site.jpg';
-import Beverages1 from '../../../Images/Packages/beveragepackage1.jpg';
-import extraHour from '../../../Images/Packages/extrahour.jpg';
-import ballons from '../../../Images/Packages/ballonGarland.4.9.2021.jpg';
-import smores from '../../../Images/Packages/smores.4.9.201.jpg';
-import Heart from '../../../Images/Charcuterie/VDay Dessert Box2.jpg';
 
-class Addon extends Component {
-	render() {
-		return (
-			<div className='addons'>
-				<Container>
-					<Row
-						style={{
-							alignItems: 'center',
-							justifyContent: 'center',
-							textAlign: 'center',
-						}}>
-						<div className='locationsHeader'>
-							<h5
-								style={{
-									marginTop: '1em',
-									fontFamily: 'Montserrat',
-									fontWeight: '500',
-									letterSpacing: '0.1em',
-									background: '#727272',
-									color: 'white',
-									padding: '0.4em',
-								}}>
-								STEP 4: SELECT ADDITIONAL OPTIONS
-							</h5>
-						</div>
-					</Row>
-				</Container>
-				<Container>
-					<Row
-						style={{
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							textAlign: 'center',
-						}}>
-						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<p className='serviceHeader'>Cozy Blanket Basket</p>
-							<Image className='addonimg' src={blanket} />
-							<h5 className='pricing'> $25</h5>
-						</Col>
-						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<p className='serviceHeader'>Beverage Package for 2</p>
-							<Image className='addonimg' src={Beverages} />
-							<h5 className='pricing'>$30</h5>
-						</Col>
-						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<p className='serviceHeader'>
-								Additional Beverages for One Guest
-							</p>
-							<Image className='addonimg' src={Beverages1} />
-							<h5 className='pricing'>$5</h5>
-						</Col>
-						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<p className='serviceHeader'>Teepee Set-Up</p>
-							<Image className='addonimg' src={Teepee} />
-							<h5 className='pricing'>$40</h5>
-						</Col>
-					</Row>
-				</Container>
-				<Container>
-					<Row
-						style={{
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							textAlign: 'center',
-						}}>
-						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<p className='serviceHeader'>Posh Umbrella with Tassels</p>
-							<Image className='addonimg' src={Umbrella} />
-							<h5 className='pricing'>$45</h5>
-						</Col>
-						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<p className='serviceHeader'>Dessert Platter </p>
-							<Image className='addonimg' src={Dessert} />
-							<h5 className='pricing'>$40</h5>
-						</Col>
-						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<p className='serviceHeader'>Cheese Charcuterie Box</p>
-							<Image className='addonimg' src={cheeseBox} />
-							<h5 className='pricing'>$50</h5>
-						</Col>
-						<Col xs={6} sm={6} md={3} lg={3} className='addonCol'>
-							<p className='serviceHeader'>Vegan/Vegetarian Charcuterie Box</p>
-							<Image className='addonimg' src={Multi} />
-							<h5 className='pricing'>$45</h5>
-						</Col>
-					</Row>
-				</Container>
-				<Container>
-					<Row
-						style={{
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							textAlign: 'center',
-						}}>
-						<Col xs={6} sm={6} md={4} lg={3} className='addonCol'>
-							<p className='serviceHeader'>Meat & Cheese Charcuterie Box</p>
-							<Image className='addonimg' src={MeatCheese} />
-							<h5 className='pricing'>$55</h5>
-						</Col>
-						<Col xs={6} sm={6} md={4} lg={3} className='addonCol'>
-							<p className='serviceHeader'>Live Music Entertainment</p>
-							<Image className='addonimg' src={Music} />
-							<h5 className='pricing'>$300+</h5>
-						</Col>
-						<Col xs={6} sm={6} md={4} lg={3} className='addonCol'>
-							<p className='serviceHeader'>Additional Hour for Picnic</p>
-							<Image className='addonimg' src={extraHour} />
-							<h5 className='pricing'>$75</h5>
-						</Col>
-						<Col xs={6} sm={6} md={4} lg={3} className='addonCol'>
-							<p className='serviceHeader'>BlueTooth Speaker</p>
-							<Image className='addonimg' src={speaker} />
-							<h5 className='pricing'>$10</h5>
-						</Col>
-					</Row>
-				</Container>
-				<Container>
-					<Row
-						style={{
-							alignItems: 'center',
-							justifyContent: 'center',
-							textAlign: 'center',
-							marginTop: '20px',
-						}}>
-						<Col sm={12} lg={4}>
-							<Image
-								fluid
-								src={ballons}
-								alt='balloons'
-								className='balloonImage'
-								style={{ width: '75%', margin: '5px' }}
-							/>
-
-							<p className='serviceHeader'>
-								One Balloon Garland for Teepee Set-Up
-								<span style={{
-									fontSize:'0.7em',
-								}}>(Teepee not included)</span>
-							</p>
-
-							<h5 className='pricing'>$225</h5>
-						</Col>
-						<Col sm={12} lg={4}>
-							<Image
-								fluid
-								src={smores}
-								alt='balloons'
-								className='balloonImage'
-								style={{ width: '75%', margin: '5px' }}
-							/>
-							<p className='serviceHeader'>Smores Boxes</p>
-							<h5 className='pricing'>
-								$50{' '}
-								<span
-									style={{
-										color: 'white',
-										fontSize: '0.5em',
-									}}>
-									(serves up to 4 guests)
-								</span>{' '}
-								$150{' '}
-								<span
-									style={{
-										color: 'white',
-										fontSize: '0.5em',
-									}}>
-									(serves up to 12 guests)
-								</span>
-							</h5>
-						</Col>
-						<Col sm={12} lg={4}>
-							<Image
-								fluid
-								src={Heart}
-								alt='Heart Shaped Box'
-								className='balloonImage'
-								style={{ width: '75%', margin: '5px' }}
-							/>
-							<p className='serviceHeader'>Heart Shaped Box</p>
-							<h5 className='pricing'>$45</h5>
-						</Col>
-					</Row>
-					<Row
-						style={{
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'center',
-							textAlign: 'center',
-						}}>
-						<p
+const Addon = () => {
+	return (
+		<>
+			<Container>
+				<Row className='headerContainer'>
+					<div className='locationsHeader'>
+						<h5
 							style={{
+								marginTop: '1em',
 								fontFamily: 'Montserrat',
 								fontWeight: '500',
-								color: '#f7901e',
-								marginTop: '20px',
+								letterSpacing: '0.1em',
+								background: '#727272',
+								color: 'white',
+								padding: '0.4em',
 							}}>
-							20% Service Fee,Sales tax & Applicable Delivery Fee will be added
-							to Pricing
-						</p>
-					</Row>
-				</Container>
-			</div>
-		);
-	}
-}
+							STEP 4: SELECT ADDITIONAL OPTIONS
+						</h5>
+					</div>
+				</Row>
+			</Container>
+			<Container fluid>
+				<Row>
+					<Col>
+						<CardDeck className='border-0'>
+							{addOnData.map((data, key) => {
+								return (
+									<div
+										className='col-md-6 col-lg-4 col-xl-3'
+										style={{
+											display: 'flex',
+											border: 'none',
+											justifyContent: 'center',
+										}}>
+										<Card
+											style={{ width: '22rem',
+										border:'none' }}
+											className='addon__card'
+											key={key}>
+											<p className='addon__title'>{data.title}</p>
+											<div>
+												<Image
+													className='addon__image'
+													src={data.image}
+													alt={data.alt}
+												/>
+											</div>
+											<p className='addon__price'>${data.price}</p>
+											<p className='addon__description'>{data.description}</p>
+										</Card>
+									</div>
+								);
+							})}
+						</CardDeck>
+					</Col>
+				</Row>
+			</Container>
+		</>
+	);
+};
 
 export default Addon;
